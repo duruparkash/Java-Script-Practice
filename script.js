daccd1d04eb44de1314be91e6f82cc65f333635e
Duru Parkash <duruparkash001@gmail.com>
2025-12-19 20:53:52 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,96 @@
+// 1st
+
+alert("Welcome to our website!");
+// 2nd
+
+alert("Error! Please enter a valid password.");
+// 3rd
+
+alert("Welcome to JS Land...\nHappy Coding!");
+// 4th
+
+alert("Welcome to JS Land...");
+alert("Prevent this page from creating additional dialogs.");
+
+// 5h
+var username;
+
+// 6th
+var myName = "John Doe";
+
+// 7th
+
+var message;
+
+message = "Hello World";
+
+alert(message);
+
+// 8th
+
+var studentName = "Jhone Doe";
+var studentAge = "15 years old";
+var studentCourse = "Certified Mobile Application Development";
+
+alert(studentName);
+alert(studentAge);
+alert(studentCourse);
+
+// 9th
+
+var pizzaPattern = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+
+alert(pizzaPattern);
+
+// 10th
+
+var email = "example@example.com";
+
+alert("My email address is " + email);
+
+// 11th
+
+var book = "A smarter way to learn JavaScript";
+
+alert("I am trying to learn from the Book " + book);
+
+// 12th
+
+document.write("Yah! I can write HTML content through JavaScript");
+
+// 13th
+
+var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+
+alert(design);
+
+document.write(design);
+
+// 14 th
+var age = 15;
+alert("I am " + age + " years old");
+
+// 15th
+var visits = 14;
+alert("You have visited this site " + visits + " times");
+
+// 16th
+
+var birthYear = 1990;
+document.write("My birth year is " + birthYear + "<br>");
+document.write("Data type of my declared variable is number");
+
+// 17th
+var name, age, country;
+
+// 18th
+
+document.write("<h1>Rules for naming JS variables</h1>");
+document.write(
+  "Variable names can only contain letters, numbers, $ and _. For example : $my_1stVariable <br>"
+);
+document.write(
+  "Variables must begin with a letter, $ or _. For example : $name, _name or name <br>"
+);
+document.write("Variable names are case sensitive <br>");
+document.write("Variable names should not be JS keywords");
